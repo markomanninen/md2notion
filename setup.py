9337de70089cbe9a5f65ac69cf342f3f4b78c9df
Noteable Kernel <engineering@noteable.io>
2023-08-22 17:17:28 +0000

    Commit from Noteable
    synced changes with remote storage

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 
 from setuptools import setup, find_packages
+
 setup(
     name='md2notionpage',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'notion-client'
+        'notion-client',
+        'mistune',
+        'html2text'
     ],
-    author='Marko T. Manninen',
-    description='A package to convert Markdown text to Notion data structure and publish a new page under the parent page',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
+    entry_points={
+        'console_scripts': [
+            'md2notionpage=cli:main',
+        ],
+    },
 )
